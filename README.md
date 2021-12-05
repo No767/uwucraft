f035823622005041464df8968c155cf3d0bda430
No767 <winterpiece104@gmail.com>
2021-12-05 13:00:14 -0800

    Init Project

---
+++ README.md
@@ -0,0 +1,11 @@
+# uwucraft
+
+adds some uwu stuff
+
+This mod is designed to add uwu items in the game. 
+
+Required Dependencies:
+
+- JDK 16 or 17 
+- Gradle 7.3
+- IntelliJ IDEA
---
+++ src/main/java/uwucraft/client/uwucraftClient.java
@@ -0,0 +1,13 @@
+package uwucraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class uwucraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
